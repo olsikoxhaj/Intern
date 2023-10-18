3aad945722d8ef46b9c349435cd9afd091d8a7ca
olsikoxhaj99 <olsikoxhaj99@gmail.com>
2023-10-18 11:22:42 +0200

    check

---
+++ Intern/TestCase/AddtoCart.cs
@@ -0,0 +1,20 @@
+﻿using Intern.PageObject;
+using Intern.SetUP;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Intern.TestCase
+{
+    public class AddtoCart : SetUp
+    {
+        public AddtoCartPage addtoCart;
+
+        [Test]
+        public void LogInAddtoCart()
+        {
+            addtoCart = new AddtoCartPage (driver);
+            driver.Navigate().GoToUrl(Constant.BaseUrl);
+            addtoCart.LogInAddtoCart();
+        }
+    }
+}
